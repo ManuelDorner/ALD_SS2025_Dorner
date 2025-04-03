c661c477b486eda2de5bbc9746b720a66860cfa7
Manuel Dorner <s2410564004@fhooe.at>
2025-04-03 09:41:19 +0200

    Hashtabel

--- Hashtable/Hashtable.cs
+++ Hashtable/Hashtable.cs
@@ -10,31 +10,121 @@ namespace Hashtable
 {
     public class Hashtable<K, V>
     {
-        
-        private ArrayList.ArrayList<SinglyLinkedList.SinglyLinkedList<Tuple<K, V>>> m_container;
+
+        public ArrayList.ArrayList<SinglyLinkedList.SinglyLinkedList<Tuple<K, V>>> m_container;
+        int count = 0;
+        public Hashtable(int size)
+        {
+            m_container = new ArrayList.ArrayList<SinglyLinkedList.SinglyLinkedList<Tuple<K, V>>>(size);
+            for (int i = 0; i < size; i++)
+            {
+                m_container.Add(new SinglyLinkedList.SinglyLinkedList<Tuple<K, V>>());
+            }
+        }
 
         public void put(K key, V value)
-        { 
-            key.GetHashCode();
-            
-        }
-        public V get(K key)
         {
-            return default(V);
+            key.GetHashCode();
+            int index = key.GetHashCode() % m_container.Count();
+            if (m_container[index] == null)
+            {
+                m_container[index] = new SinglyLinkedList.SinglyLinkedList<Tuple<K, V>>();
+            }
+            for (int i = 0; i < m_container[index].Count(); i++)
+            {
+                if (m_container[index].FindByIndex(i).Item1.Equals(key))
+                {
+                    m_container[index].Remove(m_container[index].FindByIndex(i));
+                    m_container[index].Add(new Tuple<K, V>(key, value));
+                    
+                    return;
+                }
+
+                m_container[index].Add(new Tuple<K, V>(key, value));
+                count++;
+            }
+            if (LoadFactor() > 1.5)
+            {
+                Rehash();
+            }
+
         }
 
         public bool get(K key, out V value)
         {
-            value = default(V);
-            return true;
+            int index = key.GetHashCode() % m_container.Count();
+            for (int i = 0; i < m_container[index].Count(); i++)
+            {
+                if (m_container[index].FindByIndex(i).Item1.Equals(key))
+                {
+                    value = m_container[index].FindByIndex(i).Item2;
+                    return true;
+                }
+            }
+            value = default;
+            return false;
         }
-        public override int GetHashCode()
+
+        public bool Remove(K key)
         {
-            return base.GetHashCode();
+            int index = key.GetHashCode() % m_container.Count();
+            for (int i = 0; i < m_container[index].Count(); i++)
+            {
+                if (m_container[index].FindByIndex(i).Item1.Equals(key))
+                {
+                    count--;
+                    return m_container[index].Remove(m_container[index].FindByIndex(i));
+
+                }
+            }
+            return false;
         }
-        public override bool Equals(object obj)
+        public bool ContainsKey(K key)
         {
-            return base.Equals(obj);
+            int index = key.GetHashCode() % m_container.Count();
+            for (int i = 0; i < m_container[index].Count(); i++)
+            {
+                if (m_container[index].FindByIndex(i).Item1.Equals(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public float LoadFactor()
+        {
+            return count / m_container.Count();
+
+        }
+
+        private void Rehash()
+        {
+            int newSize = m_container.Count() * 2;
+            var oldContainer = m_container;
+            m_container = new ArrayList.ArrayList<SinglyLinkedList.SinglyLinkedList<Tuple<K, V>>>(newSize);
+
+            // Neue Buckets initialisieren
+            for (int i = 0; i < newSize; i++)
+            {
+                m_container[i] = new SinglyLinkedList.SinglyLinkedList<Tuple<K, V>>();
+            }
+
+            // Alle alten Werte neu einfügen
+            count = 0;
+            for (int i = 0; i < oldContainer.Count(); i++)
+            {
+                if (oldContainer[i] != null)
+                {
+                    for (int j = 0; j < oldContainer[i].Count(); j++)
+                    {
+                        var item = oldContainer[i].FindByIndex(j);
+                        put(item.Item1, item.Item2);
+                    }
+                }
+            }
         }
     }
 }
+
---
+++ Test_Hashtable/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Test_Hashtable")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test_Hashtable")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0283dd60-54e7-4e63-863b-a8161eed291a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
